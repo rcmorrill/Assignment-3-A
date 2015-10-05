ee008360ec5758215a44c0ed6284a56e1bae24be
rcmorrill <rcmorrill@gmail.com>
2015-10-05 12:34:54 -0400

    Completed assignment 3A

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ var plot = d3.select('.canvas')
 	.attr('height',height+margin.t+margin.b)
 	.append('g')
 	.attr('class','plot')
-	//.attr('transform','translate()') --> complete this line here
+	.attr('transform', 'translate ('+margin.l+','+margin.r+')');
 
 
 //Start with 100 symbols
@@ -24,8 +24,50 @@ var numOfSymbols = 100;
 // --> type: circle or square
 // --> color
 var symbols = [];
+var test = [];
+
+function randBetween (min, max) {
+	return Math.random()*(max - min) + min;
+}
+
+
+for (i=0; i<numOfSymbols; i++){
+	var newObject = {
+		xPos: randBetween(0,width),
+		yPos: randBetween(0,height),
+		size: Math.round(randBetween(0,101)),
+		shape: Math.round(Math.random()),
+		color: {r:Math.round(randBetween (0, 255)), g:Math.round(randBetween (0, 255)), b:Math.round(randBetween (0, 255))}
+	};
+	symbols.push(newObject)
+	test.push(newObject.shape)
+
+}
+
+console.log(symbols);
+console.log(test);
 
 //With the array you've created and populated, draw circles to represent these symbols
+
+
+
 symbols.forEach(function(symbol){
+	if (symbol.shape == 1) {
+		plot
+			.append('circle')
+			.attr('cx', symbol.xPos)
+			.attr('cy', symbol.yPos)
+			.attr('r',symbol.size)
+			.attr('fill','rgb('+symbol.color.r+','+symbol.color.g+','+symbol.color.b+')');
 	
+	}else if (symbol.shape == 0) {
+		plot
+			.append('rect')
+			.attr('x', symbol.xPos)
+			.attr('y', symbol.yPos)
+			.attr('width',symbol.size)
+			.attr('height',symbol.size)
+			.attr('fill','rgb('+symbol.color.r+','+symbol.color.g+','+symbol.color.b+')');
+	}
 })
+
